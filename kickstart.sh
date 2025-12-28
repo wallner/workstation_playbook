91013929f28905584791a20a9046baf89ea515c8
Florian Wallner <florian.wallner@proton.me>
2025-12-28 21:13:29 +0100

    Clean up the playbook
    This massive commit cleans up the playbook:
    - It inverts the become logic: run as user and only elevate privileges
      when needed
    - use `import_tasks` instead of `include_tasks` it's way faster
    - split up the flatpack installations into apps and sdk extensions
    - use ansibles `command` task to execute the flatpak command for the
      sdk installation. That was the only way to not have the sdk
      installation always report as 'changed'
    - make the installation of go binaries stop reporting as changed, no
      matter if that's true or not
    - have the installation of the dotfiles not use restow
    - silence the reporting as changed for rebuilding ba-caches and re-build
      them all the time
    - White space clean-ups

--- kickstart.sh
+++ kickstart.sh
@@ -3,4 +3,4 @@ sudo dnf install git
 sudo dnf install ansible
 ansible-galaxy collection install community.general -p collections
 ansible-galaxy role install luizgavalda.gnome_extensions -p collections 
-ansible-playbook -K playbook.yml  
+ansible-playbook -K playbook.yml
