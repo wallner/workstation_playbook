4fef06d061e6979a7adbc503b3ecf1e8ce5ae14b
Florian Wallner <florian.wallner@proton.me>
2026-01-09 11:38:56 +0100

    Make user configurable

--- kickstart.sh
+++ kickstart.sh
@@ -1,6 +1,17 @@
 #!/bin/env sh
-sudo dnf install git
-sudo dnf install ansible
+TARGET_USER=${1:-$(whoami)}
+
+# Install dependencies
+sudo dnf install -y git ansible
+
+# Install collections and roles
 ansible-galaxy collection install community.general -p collections
 ansible-galaxy role install luizgavalda.gnome_extensions -p roles
-ansible-playbook -K playbook.yml --tags upgrade
+
+# Run playbook. Only ask for sudo password if in an interactive terminal.
+ANSIBLE_FLAGS="--tags upgrade"
+if [ -t 0 ]; then
+    ANSIBLE_FLAGS="$ANSIBLE_FLAGS -K"
+fi
+
+ansible-playbook $ANSIBLE_FLAGS playbook.yml -e "user=$TARGET_USER"
